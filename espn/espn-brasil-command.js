a7011032cfccbc11fc48093d061600482bc5074f
Clauuber Stipkovic <clauber.halic@gmail.com>
2011-07-18 01:21:42 -0300

    Updateing the ESPN favicon URL

--- espn/espn-brasil-command.js
+++ espn/espn-brasil-command.js
@@ -1,54 +1,60 @@
 /* This Commmand was maked to Mozilla Ubiquity 0.6.x */
 CmdUtils.CreateCommand({
-  names: ["espn-brasil", "espn-br"],
-  icon: "http://espn.estadao.com.br/img/fe/favicon.gif",
-  description: "Mostra as ultimas noticias do site ESPN-Brasil",
-  help: "Digite espn-brasil ou espn-br e o termo que você quer buscar!",
+  names: ['espn-br', 'espn-brasil'],
+  icon: 'http://espn.estadao.com.br/img/fe/favicon.gif',
+  description: 'Mostra as ultimas noticias do site ESPN.com.br',
+  help: 'Digite espn-br e o termo que você quer buscar!',
   author: {
-    name: "Clauber Stipkovic", 
-    email: "clauber.halic@gmail.com"
+    name: 'Clauber Stipkovic', 
+    email: 'clauber.halic@gmail.com'
   },
-  license: "GPL",
-  homepage: "http://github.com/ClauberStipkovic/espn-br-ubiquity-command",
+  license: 'GPL',
+  homepage: 'http://github.com/ClauberStipkovic/ubiquity-commands',
   arguments: [{
     role: 'object', 
-    nountype: noun_arb_text
+    nountype: noun_arb_text, 
+    label: 'search term'
   }],
-  /* Function that makes Ajax request at ESPN-Brasil RSS url */
-  _getEspnBrasilNews: function getEspnBrasilNews__getEspnBrasilNews(pblock, args) {
+  /* Function that makes Ajax request at ESPN.com.br RSS url */
+  _getEspnBrNews: function getEspnBrNews__getEspnBrNews(pblock, args) {
     pblock.innerHTML = 
-      "Procurando no ESPN Brasil por <b>"+ args.object.text + 
-      "</b>.<br />";
+      'Procurando no ESPN Brasil por <b>'+ args.object.text + 
+      '</b>.<br />';
+
     var urlSearchESPN_BR = 
       "http://espn.estadao.com.br/rss/"+ args.object.text;
     $.ajax({
       url: urlSearchESPN_BR,
       success: function (responseData) {
-        pblock.innerHTML += "<dl>";
-	$(responseData).find("item").each( function () {
-          pblock.innerHTML += 
-            "<dt style=\"font-size: 0.9em\">"+
-              "<b><a href="+ $(this).find("link").text() +">"+ 
-                $(this).find("title").text() +"</a></b>"+
-            "</dt>";
+        if ($(responseData).find('item').length > 1) {
+          pblock.innerHTML += "<dl>";
+          $(responseData).find("item").each( function () {
+            pblock.innerHTML += 
+              "<dt style=\"font-size: 0.9em\">"+
+                "<b><a href="+ $(this).find("link").text() +">"+ 
+                  $(this).find("title").text() +"</a></b>"+
+              "</dt>";
 	  
-          pblock.innerHTML += 
-            "<dd style=\"font-size: 0.8em\">"+ 
-              $(this).find("description").text() +"</dd><br />";
+            pblock.innerHTML += 
+              "<dd style=\"font-size: 0.8em\">"+ 
+                $(this).find("description").text() +"</dd><br />";
+          )}; 
           pblock.innerHTML += "</dl>";
-        )};
+        } else {
+          pblock.innerHTML = 'Assunto não encontrado no ESPN.com.br';
+        }
       },
       error: function () {
-        pblock.innerHTML = "Assunto não encontrado no ESPN Brasil.";
+        pblock.innerHTML = "Assunto não encontrado no ESPN.com.br";
       }
     });
   },
   preview: function preview (pblock, args) {
     if (!args.object.text || args.object.text < 2) {
-      pblock.innerHTML = "Procura noticias no site ESPN-Brasil.";
+      pblock.innerHTML = "Procura noticias no site ESPN.com.br";
       return;
     }
-    this._getEspnBrasilNews(pblock, args);
+    this._getEspnBrNews(pblock, args);
   },
   execute: function execute (args) {
     Utils.openUrlInBrowser("http://espn.estadao.com.br/"+ args.object.text);
--- espn/espn-com-command.js
+++ espn/espn-com-command.js
@@ -1,48 +1,63 @@
-/* 
-	This Commmand was maked to Mozilla Ubiquity 0.5.x 
-	Show the last sport news at ESPN web site
-*/
+/* This Commmand was maked to Mozilla Ubiquity 0.6.x */
 CmdUtils.CreateCommand({
-	names: ['espn', 'espn-com'],
-	icon: 'http://espnbrasil.terra.com.br/img/fe/favicon.gif',
-	description: 'Shows the last news about sports from ESPN.com web site',
-	help: 'Type espn or espn-com and the term that you want to search!',
-	author: {name: 'Clauber Stipkovic', email: 'clauber.halic@gmail.com'},
-	license: 'GPL',
-	homepage: 'http://github.com/ClauberStipkovic/ubiquity-commands',
-	arguments: [{role: 'object', nountype: noun_arb_text, label: 'search term'}], 
-	/* Function that makes Ajax request at ESPN.com RSS url */
-	_getEspnComNews: function getEspnComNews__getEspnComNews(pblock, args) {
-		pblock.innerHTML = 'Searching at ESPN.com for <b>' + args.object.text + '</b>.<br /><br />';
-	 
-		var urlSearchESPN_COM = 'http://search.espn.go.com/rss/'+ args.object.text;
-		$.ajax({
-			url: urlSearchESPN_COM,
-			success: function(responseData) {
-				if($(responseData).find('item').length > 1) {
-					pblock.innerHTML += '<dl>';
-					$(responseData).find('item').each(function() {
-						pblock.innerHTML += '<dt style=\"font-size: 0.9em\"><b><a href='+ $(this).find('link').text() +'>'+ $(this).find('title').text() + '</a></b></dt>';
-						pblock.innerHTML += '<dd style=\"font-size: 0.8em\">'+ $(this).find('description').text() +'</dd><br />';
-					});
-					pblock.innerHTML += '</dl>';
-				} else {
-					pblock.innerHTML = 'Subject not found at ESPN.com.';
-				}
-			},
-			error: function() {
-				pblock.innerHTML = 'Subject not found at ESPN.com.';
-			}
-		});
-	},
-	preview: function preview(pblock, args) {
-		if(!args.object.text || args.object.text < 2) {
-			pblock.innerHTML = 'Search news about sports at ESPN.com.';
-			return;
-		}
-		this._getEspnComNews(pblock, args);
-	},
-	execute: function execute(args) {
-		Utils.openUrlInBrowser('http://search.espn.go.com/'+ args.object.text);
+  names: ['espn', 'espn-com'],
+  icon: 'http://espn.estadao.com.br/img/fe/favicon.gif',
+  description: 'Shows the last news about sports from ESPN.com',
+  help: 'Type espn or espn-com and the term that you want to search!',
+  author: {
+    name: 'Clauber Stipkovic', 
+    email: 'clauber.halic@gmail.com'
+  },
+  license: 'GPL',
+  homepage: 'http://github.com/ClauberStipkovic/ubiquity-commands',
+  arguments: [{
+    role: 'object', 
+    nountype: noun_arb_text, 
+    label: 'search term'
+  }], 
+  /* Function that makes Ajax request at ESPN.com RSS url */
+  _getEspnComNews: function getEspnComNews__getEspnComNews(pblock, args) {
+    pblock.innerHTML = 
+      'Searching at ESPN.com for <b>' + args.object.text + 
+      '</b>.<br /><br />';
+ 
+    var urlSearchESPN_COM = 
+      'http://search.espn.go.com/rss/'+ args.object.text;
+    $.ajax({
+      url: urlSearchESPN_COM,
+      success: function(responseData) {
+        if ($(responseData).find('item').length > 1) {
+          pblock.innerHTML += '<dl>';
+	  $(responseData).find('item').each(function() {
+	    pblock.innerHTML += 
+              '<dt style=\"font-size: 0.9em\">'+
+                '<b><a href='+ $(this).find('link').text() +'>'+
+                $(this).find('title').text() + '</a></b>'+
+              '</dt>';
+	    
+          pblock.innerHTML += 
+            '<dd style=\"font-size: 0.8em\">'+ 
+              $(this).find('description').text() +'</dd><br />';
+	  });
+	  pblock.innerHTML += '</dl>';
+	} else {
+	  pblock.innerHTML = 'Subject not found at ESPN.com.';
 	}
+      },
+      error: function() {
+        pblock.innerHTML = 'Subject not found at ESPN.com.';
+      }
+    });
+  },
+  preview: function preview(pblock, args) {
+    if (!args.object.text || args.object.text < 2) {
+      pblock.innerHTML = 'Search news about sports at ESPN.com.';
+      return;
+    }
+    this._getEspnComNews(pblock, args);
+  },
+  execute: function execute(args) {
+    Utils.openUrlInBrowser('http://search.espn.go.com/'+ args.object.text);
+  }
 });
+
