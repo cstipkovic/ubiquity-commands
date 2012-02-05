7a1c2f3b5f9906476584acd9acd32db2e916a6ce
Clauuber Stipkovic <clauber.halic@gmail.com>
2012-02-05 15:56:31 -0200

    Ubiquity command for ESPN International fixed

--- espn/espn-com-command.js
+++ espn/espn-com-command.js
@@ -1,63 +1,65 @@
 /* This Commmand was maked to Mozilla Ubiquity 0.6.x */
 CmdUtils.CreateCommand({
-  names: ['espn', 'espn-com'],
-  icon: 'http://espn.estadao.com.br/img/fe/favicon.gif',
-  description: 'Shows the last news about sports from ESPN.com',
-  help: 'Type espn or espn-com and the term that you want to search!',
+  names: ["espn", "espn-com"],
+  description: "Shows the last news about sports from ESPN.com",
+  help: "Type espn or espn-com and the term that you want to search!",
   author: {
-    name: 'Clauber Stipkovic',
-    email: 'clauber.halic@gmail.com'
+    name: "Clauber Stipkovic",
+    email: "clauber.halic@gmail.com",
+    homepage: "http://clauber.coffeebreakers.org"
   },
-  license: 'GPL',
-  homepage: 'http://github.com/ClauberStipkovic/ubiquity-commands',
+  license: "GPL",
+  homepage: "http://github.com/ClauberStipkovic/ubiquity-commands",
+  icon: "http://espn.estadao.com.br/img/fe/favicon.gif",
   arguments: [{
-    role: 'object',
+    role: "object",
     nountype: noun_arb_text,
-    label: 'search term'
+    label: "search term"
   }],
   /* Function that makes Ajax request at ESPN.com RSS url */
   _getEspnComNews: function getEspnComNews__getEspnComNews(pblock, args) {
     pblock.innerHTML = 'Searching at ESPN.com for <b>'+
-                          args.object.text +
-                       '</b>.<br /><br />';
+                          args.object.text +'</b>.<br /><br />';
 
-    var urlRSS = 'http://search.espn.go.com/rss/'+ args.object.text;
+    var seachString = args.object.text;
+    var urlSearchESPN = 'http://search.espn.go.com/rss/'+ 
+                        seachString.replace(/\s/, "-");
 
-    CmdUtils.previewAjax(pblock, {
-      type: "GET",
-      url: urlRSS,
-      dataType: "xml",
-      success: function (data) {
-      $.each(data.parse.text, function(i, item) {
-        if ($(responseData).find('item').length > 1) {
-          pblock.innerHTML += '<dl>';
-	  $(responseData).find('item').each(function() {
-	    pblock.innerHTML += '<dt style=\"font-size: 0.9em\">'+
-                  '<b><a href='+ $(this).find('link').text() +'>'+
-                    $(this).find('title').text() + '</a></b>'+
-                '</dt>';
+    $.ajax({
+      url: urlSearchESPN,
+      success: function(responseData) {
+        var itemsNews = $(responseData).find("item");
 
-            pblock.innerHTML +=
-                '<dd style=\"font-size: 0.8em\">'+
-                  $(this).find('description').text() +'</dd><br />';
-	          });
-	          pblock.innerHTML += '</dl>';
-	        } else {
-	          pblock.innerHTML = 'Subject not found at ESPN.com.';
-	        }
-        });
+        if (itemsNews.length) {
+          pblock.innerHTML = "The ten lastest news about <b>"+ 
+                              args.object.text +"</b>"
+          pblock.innerHTML += "<br /><dl>";
+          for (i = 0; i < 10; i++) {
+            var itemContent = 
+              "<dt style='font-size: 0.9em'>" +
+                "<b><a href="+ $(itemsNews).eq(i).find("link").text() +">"+
+                $(itemsNews).eq(i).find("title").text() +"</a></b>"+
+              "</dt>"+
+              "<dd style='font-size: 0.8em'>"+ 
+                $(itemsNews).eq(i).find("description").text() +
+              "</dd><br />";
+            pblock.innerHTML += itemContent;
+          }
+          pblock.innerHTML += "</dl>";
+        } else {
+          pblock.innerHTML = "Subject not found at ESPN.com";
+        }
 	    }
 	  });
   },
   preview: function preview(pblock, args) {
     if (!args.object.text || args.object.text < 2) {
-      pblock.innerHTML = 'Search news about sports at ESPN.com.';
+      pblock.innerHTML = "Search news about sports at ESPN.com";
       return;
     }
     this._getEspnComNews(pblock, args);
   },
   execute: function execute(args) {
-    Utils.openUrlInBrowser('http://search.espn.go.com/'+ args.object.text);
+    Utils.openUrlInBrowser("http://search.espn.go.com/"+ args.object.text);
   }
 });
-
